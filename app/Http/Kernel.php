09b4ffac999a46d55971e89de41da904dbfd637e
achmadjp <achmadjp7@gmail.com>
2020-05-22 12:51:11 +0700

    passport

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,9 +15,9 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         // \App\Http\Middleware\TrustHosts::class,
+        \App\Http\Middleware\CheckForMaintenanceMode::class,
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
-        \App\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'CheckClientCredentials' => \App\Http\Middleware\CheckClientCredentials::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('login', 'UserController@login');
+Route::post('register', 'UserController@register');
+Route::post('refreshtoken', 'UserController@refreshToken');
+
+Route::get('/unauthorized', 'UserController@unauthorized');
+Route::group(['middleware' => ['CheckClientCredentials', 'auth:api']], function(){
+    Route::post('logout', 'UserController@logout');
+    Route::post('details', 'UserController@details');
 });
